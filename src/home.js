44fd6eadb0b43fba348e83ffb2b06f1c26dda48d
unknown <jsmileydev@gmail.com>
2019-09-30 17:23:37 -0500

    Layout for game data search

--- src/home.js
+++ src/home.js
@@ -4,28 +4,29 @@ class Home extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			game: ''
+			game: '',
+			plat: ''
 		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-		//this.fetchResults = this.fetchResults.bind(this);
+		this.inputGameTitle = this.inputGameTitle.bind(this);
+		this.submitGameTitle = this.submitGameTitle.bind(this);
+		this.fetchTitleResults = this.fetchTitleResults.bind(this);
+		this.inputGameTitleData = this.inputGameTitleData.bind(this);
+		this.inputGamePlatData = this.inputGamePlatData.bind(this);
+		this.submitGameData = this.submitGameData.bind(this);
+		this.fetchDataResults = this.fetchDataResults.bind(this);
 	}
 
-	handleChange(e) {
+	inputGameTitle(e) {
 		var search = e.target.value;
 		this.setState({ game: search });
 	}
 
-	handleClick() {
+	submitGameTitle() {
 		console.log(this.state.game);
-		this.fetchResults();
+		this.fetchTitleResults();
 	}
 
-	/*componentDidMount() {
-		this.fetchResults();
-	}*/
-
-	fetchResults() {
+	fetchTitleResults() {
 		// Use preventDefault() to stop the form submitting
 		var data = null;
 
@@ -52,9 +53,66 @@ class Home extends React.Component {
 			}
 		});
 
-		xhr.open("GET", "https://chicken-coop.p.rapidapi.com/games?title=" + url);
-		xhr.setRequestHeader("x-rapidapi-host", "chicken-coop.p.rapidapi.com");
-		xhr.setRequestHeader("x-rapidapi-key", "5d0b4bd3ccmshb5942bd75f3f8b8p1c8bb5jsnb3247ba29bde");
+		xhr.open('GET', 'https://chicken-coop.p.rapidapi.com/games?title=' + url);
+		xhr.setRequestHeader('x-rapidapi-host', 'chicken-coop.p.rapidapi.com');
+		xhr.setRequestHeader('x-rapidapi-key', '5d0b4bd3ccmshb5942bd75f3f8b8p1c8bb5jsnb3247ba29bde');
+
+		xhr.send(data);
+	}
+
+	inputGameTitleData(e) {
+		var search = e.target.value;
+		this.setState({ game: search });
+	}
+
+	componentDidMount() {
+		var platSearch = document.getElementById('search-plat-data').value;
+		this.setState({
+			plat: platSearch
+		})
+	}
+
+	
+
+	inputGamePlatData(e) {
+		var platSearch = e.target.value;
+		this.setState({ plat: platSearch });
+	}
+
+	submitGameData() {
+		this.fetchDataResults();
+	}
+
+	fetchDataResults() {
+		var data = null;
+
+		var title = this.state.game;
+		var plat = this.state.plat;
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener('readystatechange', function() {
+			if (this.readyState === this.DONE) {
+				console.log(this.responseText);
+				var gameobj = JSON.parse(this.responseText);
+				console.log(gameobj.result);
+				/*var html = '';
+				gameobj.result.forEach(function(val) {
+					var keys = Object.keys(val);
+					html += "<div class=''>";
+					keys.forEach(function(key) {
+						html += '<strong>' + key + '</strong>: ' + val[key] + '<br>';
+					});
+					html += '</div><br>';
+				});*/
+				document.getElementById('game-results').innerHTML = gameobj.result;
+			}
+		});
+
+		xhr.open('GET', 'https://chicken-coop.p.rapidapi.com/games/' + title + '?platform=' + plat);
+		xhr.setRequestHeader('x-rapidapi-host', 'chicken-coop.p.rapidapi.com');
+		xhr.setRequestHeader('x-rapidapi-key', '5d0b4bd3ccmshb5942bd75f3f8b8p1c8bb5jsnb3247ba29bde');
 
 		xhr.send(data);
 	}
@@ -63,22 +121,57 @@ class Home extends React.Component {
 		return (
 			<div>
 				<h1>Game search</h1>
+				<h4>Returns all related game titles and the platforms released under</h4>
 
 				<div className="wrapper">
 					<div className="controls">
 						<form>
 							<p>
-								<label htmlFor="search">Enter a SINGLE search term (required): </label>
+								<label htmlFor="search-title">Enter a SINGLE search term (required): </label>
 								<input
 									type="text"
-									id="search"
+									id="search-title"
 									className="search"
-									onChange={this.handleChange}
+									onChange={this.inputGameTitle}
 									required
 								/>
 							</p>
 							<p>
-								<input type="button" className="submit" onClick={this.handleClick} value="Submit search"/>
+								<input
+									type="button"
+									className="submit"
+									onClick={this.submitGameTitle}
+									value="Submit search"
+								/>
+							</p>
+						</form>
+						<form>
+							<p>
+								<label htmlFor="search-title-data">Enter a SINGLE game title (required): </label>
+								<input
+									type="text"
+									id="search-title-data"
+									className="search"
+									onChange={this.inputGameTitleData}
+									required
+								/>
+								<label htmlFor="search-plat-data">Enter a SINGLE platform (required): </label>
+								<input
+									type="text"
+									id="search-plat-data"
+									className="search"
+									value="pc"
+									onChange={this.inputGamePlatData}
+									required
+								/>
+							</p>
+							<p>
+								<input
+									type="button"
+									className="submit"
+									onClick={this.submitGameData}
+									value="Submit search"
+								/>
 							</p>
 						</form>
 					</div>
