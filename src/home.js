5f30efa1840d54cb7924fdf99a79666b61396870
unknown <jsmileydev@gmail.com>
2019-10-01 11:52:07 -0500

    Add function to sort game title search results

--- src/home.js
+++ src/home.js
@@ -9,12 +9,15 @@ class Home extends React.Component {
 		};
 		this.inputGameTitle = this.inputGameTitle.bind(this);
 		this.submitGameTitle = this.submitGameTitle.bind(this);
-		this.fetchTitleResults = this.fetchTitleResults.bind(this);
+		//this.fetchTitleResults = this.fetchTitleResults.bind(this);
 		this.inputGameTitleData = this.inputGameTitleData.bind(this);
 		this.inputGamePlatData = this.inputGamePlatData.bind(this);
 		this.submitGameData = this.submitGameData.bind(this);
-		this.fetchDataResults = this.fetchDataResults.bind(this);
+		//this.fetchDataResults = this.fetchDataResults.bind(this);
 	}
+
+	//SEARCH FOR ALL GAMES RELATED TO INPUT
+	
 
 	inputGameTitle(e) {
 		var search = e.target.value;
@@ -22,11 +25,11 @@ class Home extends React.Component {
 	}
 
 	submitGameTitle() {
-		console.log(this.state.game);
+		/*console.log(this.state.game);
 		this.fetchTitleResults();
 	}
 
-	fetchTitleResults() {
+	fetchTitleResults() {*/
 		// Use preventDefault() to stop the form submitting
 		var data = null;
 
@@ -35,17 +38,43 @@ class Home extends React.Component {
 		var xhr = new XMLHttpRequest();
 		xhr.withCredentials = true;
 
+		//Function to sort result items
+		function dynamicSort(property) {
+			var sortOrder = 1;
+			if (property[0] === '-') {
+				sortOrder = -1;
+				property = property.substr(1);
+			}
+			return function(a, b) {
+				if (sortOrder === -1) {
+					return b[property].localeCompare(a[property]);
+				} else {
+					return a[property].localeCompare(b[property]);
+				}
+			};
+		}
+
 		xhr.addEventListener('readystatechange', function() {
 			if (this.readyState === this.DONE) {
 				console.log(this.responseText);
+				//Convert responseText string to useable object
 				var gameobj = JSON.parse(this.responseText);
 				console.log(gameobj.result);
+				//Use dynamicSort to group result object by title alphabetically (otherwise seemingly random?)
+				gameobj.result = gameobj.result.sort(dynamicSort('title'));
 				var html = '';
 				gameobj.result.forEach(function(val) {
 					var keys = Object.keys(val);
 					html += "<div class=''>";
 					keys.forEach(function(key) {
-						html += '<strong>' + key + '</strong>: ' + val[key] + '<br>';
+						html +=
+							'<strong>' +
+							key.replace(/([a-z](?=[A-Z]))/g, '$1 ').replace(/^./, function(str) {
+								return str.toUpperCase();
+							}) +
+							'</strong>: ' +
+							val[key] +
+							'<br>';
 					});
 					html += '</div><br>';
 				});
@@ -65,14 +94,15 @@ class Home extends React.Component {
 		this.setState({ game: search });
 	}
 
+	//Set state of platform to default value of input field ('pc') on initial load
 	componentDidMount() {
 		var platSearch = document.getElementById('search-plat-data').value;
 		this.setState({
 			plat: platSearch
-		})
+		});
 	}
 
-	
+	// SEARCH FULL DATA FOR ONE GAME
 
 	inputGamePlatData(e) {
 		var platSearch = e.target.value;
@@ -80,10 +110,10 @@ class Home extends React.Component {
 	}
 
 	submitGameData() {
-		this.fetchDataResults();
+		/*this.fetchDataResults();
 	}
 
-	fetchDataResults() {
+	fetchDataResults() {*/
 		var data = null;
 
 		var title = this.state.game;
@@ -96,16 +126,16 @@ class Home extends React.Component {
 			if (this.readyState === this.DONE) {
 				console.log(this.responseText);
 				var gameobj = JSON.parse(this.responseText);
-				console.log(gameobj.result);
-				var html = '';
+				console.log(gameobj.result['title']);
+				/*var html = '';
 				for (var key in gameobj.result) {
 					html += "<div class=''>";
 					if(gameobj.result.hasOwnProperty(key)) {
-						html += '<strong>' + key + '</strong>: ' + gameobj.result[key] + '<br>';
+						html += '<strong>' + key.replace(/([a-z](?=[A-Z]))/g, '$1 ').replace(/^./, function(str){ return str.toUpperCase(); }) + '</strong>: ' + gameobj.result[key] + '<br>';
 					}
 					html += '</div><br>';
 				}
-				document.getElementById('game-results').innerHTML = html;
+				document.getElementById('game-results').innerHTML = html;*/
 			}
 		});
 
